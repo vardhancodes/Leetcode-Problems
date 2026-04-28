50d84b40a8aebe55eb22fccf3188793210fe018b
Viraj Vardhan Pandey <vardhanviraj05@gmail.com>
2026-04-28 20:45:23 +0530

    Time: 1 ms (24.87%) | Memory: 42 MB (95.90%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        List<Integer> sublist = new ArrayList<Integer>();
+        int []arr = {1,2,3,4,5,6,7,8,9};
+        recur(arr,k,n,list,sublist,0);
+        return list;
+    }
+    public static void recur(int[] arr, int k, int target,List<List<Integer>> list ,List<Integer> sublist , int ind)
+    {
+        if(k == 0)
+        {
+            if(target == 0)
+            {
+                list.add(new ArrayList<>(sublist));
+                
+            }
+            return;
+        }
+        
+
+        for(int i = ind ; i < arr.length ; i++)
+        {
+            if(arr[i] <= target && (i == ind || arr[i] != arr[i-1]))
+            {
+                sublist.add(arr[i]);
+                recur(arr,k-1,target-arr[i],list,sublist,i+1);
+                sublist.remove(sublist.size()-1);
+            }
+        }
+    }
+}
