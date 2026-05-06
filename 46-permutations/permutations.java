b9bca83d55ebab3d05bc67c2f4a52bd12d865b35
Viraj Vardhan Pandey <vardhanviraj05@gmail.com>
2026-05-07 01:20:46 +0530

    Time: 1 ms (86.62%) | Memory: 45.7 MB (30.09%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,36 +1,38 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
-        recur(list,nums,0);
+        boolean vis[] = new boolean[nums.length];
+        List<Integer>sublist = new ArrayList<>();
+        recur(list,sublist,nums,vis);
         return list;
     }
-    public static void recur(List<List<Integer>> list, int[] nums , int ind )
+    public static void recur(List<List<Integer>> list, List<Integer>sublist, int[] nums , boolean[] vis)
     {
-       if(ind == nums.length)
-       {    List<Integer> sublist = new ArrayList<>();
-            for(int i = 0 ; i < nums.length ; i++)
-            {
-                sublist.add(nums[i]);
-            }
-            list.add(sublist);
-
+        if(sublist.size() == nums.length)
+        {
+            list.add(new ArrayList<>(sublist));
             return;
-       }
+        }
 
-       for(int i = ind ; i < nums.length ; i++)
-       {
-            swap(nums,i,ind);
-            recur(list,nums,ind+1);
-            swap(nums,i,ind);
-       }
+        for(int i = 0 ; i < nums.length ; i++)
+        {
+            if(vis[i])
+            {
+                continue;
+            }
+            if(i > 0 && nums[i-1] == nums[i] && !vis[i-1])
+            {
+                continue;
+            }
+            vis[i] = true;
+            sublist.add(nums[i]);
+            recur(list,sublist,nums,vis);
+            sublist.remove(sublist.size()-1);
+            vis[i] = false;
+        }
     }
 
-    public static void swap(int[] nums, int start , int end)
-    {
-        int temp = nums[start];
-        nums[start] = nums[end];
-        nums[end] = temp;
-    }
+   
 
     
 }
