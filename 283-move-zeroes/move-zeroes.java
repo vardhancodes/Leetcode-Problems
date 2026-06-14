47c57406eab0a739d910fde0d1084fb963bb697d
Viraj Vardhan Pandey <vardhanviraj05@gmail.com>
2026-06-14 18:28:07 +0530

    Time: 2 ms (91.89%) | Memory: 47.9 MB (37.61%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,7 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int fill = 0;
-        int find = 0;
+        while(fill < nums.length && nums[fill] != 0)
+        {
+            fill++;
+        }
+        if(fill >= nums.length)
+        {
+            return;
+        }
+        int find = fill;
         while(find < nums.length)
         {
             if(nums[find] != 0)
