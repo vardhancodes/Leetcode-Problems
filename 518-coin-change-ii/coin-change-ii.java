7552e475874acd094bd10382c9bac02eb0cc3531
Viraj Vardhan Pandey <vardhanviraj05@gmail.com>
2026-07-04 18:29:32 +0530

    Time: 17 ms (28.92%) | Memory: 56.4 MB (51.93%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,40 +1,45 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int dp[][] = new int[coins.length+1][amount+1];
-        for(int[] row : dp)
+        int n = coins.length;
+        int a = amount;
+        for(int i = 0 ; i < n+1 ; i++)
         {
-            Arrays.fill(row,-1);
+            for(int j = 0 ; j < a+1 ; j++)
+            {
+                if(j == 0)
+                {
+                    dp[i][j] = 1;
+                }
+                else if(i == 0)
+                {
+                    dp[i][j] = 0;
+                }
+            }
+            
         }
-        return recur(coins.length,amount,coins,dp);
+
+        for(int i = 1 ; i < n+1 ; i++)
+        {
+            for(int j = 1 ; j < a+1 ; j++)
+            {
+                if(coins[i-1] <= j)
+                {
+                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+            
+        }
+
+
+        return dp[n][a];
+
+
+    
     }
 
-    public static int recur(int n, int a, int[] coins, int[][] dp)
-    {
-        if(a == 0)
-        {
-            dp[n][a] = 1;
-            return 1;
-        }
-
-        if(n == 0)
-        {
-            dp[n][a] = 0;
-            return 0;
-        }
-
-        if(dp[n][a] != -1)
-        {
-            return dp[n][a];
-        }
-
-        if(coins[n-1] <= a)
-        {
-            return dp[n][a] = recur(n,a-coins[n-1],coins,dp)+recur(n-1,a,coins,dp);
-        }
-
-        else
-        {
-            return dp[n][a] = recur(n-1,a,coins,dp);
-        }
-    }
 }
