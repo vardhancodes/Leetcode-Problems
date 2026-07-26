e6d2598320627eb131d14ab80e6c37db7b483be9
Viraj Vardhan Pandey <vardhanviraj05@gmail.com>
2026-07-26 18:25:10 +0530

    Time: 20 ms (78.45%) | Memory: 53.4 MB (89.56%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -3,36 +3,24 @@ class Solution {
         int n = text1.length();
         int m = text2.length();
         int dp[][] = new int[n+1][m+1];
+        
 
-        for(int[] rows : dp)
+        for(int i = 1 ; i < n+1 ; i++)
         {
-            Arrays.fill(rows,-1);
+            for(int j = 1 ; j < m+1 ; j++)
+            {
+                if(text1.charAt(i-1) == text2.charAt(j-1))
+                {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
         }
 
-        return recur(n,m,text1,text2,dp);
-    }
-
-    public static int recur(int n, int m, String t1, String t2, int[][] dp)
-    {
-        if(n == 0 || m == 0)
-        {
-            return 0;
-        }
-
-        if(dp[n][m] != -1)
-        {
-            return dp[n][m];
-        }
-
-        if(t1.charAt(n-1) == t2.charAt(m-1))
-        {
-            return dp[n][m] = 1+ recur(n-1,m-1,t1,t2,dp);
-        }
-
-        else
-        {
-            return dp[n][m] = Math.max(recur(n-1,m,t1,t2,dp),recur(n,m-1,t1,t2,dp));
-        }
-
+        return dp[n][m];
+    
     }
 }
