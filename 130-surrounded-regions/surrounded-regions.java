13a7527dcfe0f37b9197988cf6730b34c64bd03b
Viraj Vardhan Pandey <vardhanviraj05@gmail.com>
2026-09-16 21:19:01 +0530

    Time: 3 ms (29.52%) | Memory: 47.5 MB (86.83%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.java
@@ -0,0 +1,54 @@
+class Solution {
+    public void solve(char[][] board) {
+        boolean vis[][] = new boolean[board.length][board[0].length];
+        for(int i = 0 ; i < board.length ; i++)
+        {
+            for(int j = 0 ; j < board[0].length ; j++)
+            {
+                if(i == 0 || j == 0 || i == board.length-1 || j == board[0].length-1)
+                {
+                    if(board[i][j] == 'O' && vis[i][j] == false)
+                    {
+                        dfs(vis,board,i,j);
+                    }
+                }
+            }
+        }
+
+        for(int i = 0 ; i < board.length ; i++)
+        {
+            for(int j = 0 ; j < board[0].length ; j++)
+            {
+                if(board[i][j] == '.')
+                {
+                    board[i][j] = 'O';
+                }
+                else if(board[i][j] == 'O')
+                {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    public void dfs(boolean[][] vis, char[][] board, int i, int j)
+    {
+        vis[i][j] = true;
+        board[i][j] = '.';
+
+        int di[] = {-1,0,1,0};
+        int dj[] = {0,1,0,-1};
+
+        for(int ind = 0 ; ind < 4 ; ind++)
+        {
+            int ni = i+di[ind];
+            int nj = j+dj[ind];
+
+            if(ni >= 0 && ni < board.length && nj >= 0 && nj < board[0].length && !vis[ni][nj] && board[ni][nj] != 'X')
+            {
+                dfs(vis,board,ni,nj);
+            }
+        }
+
+    }
+}
